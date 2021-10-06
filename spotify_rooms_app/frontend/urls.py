352cb18c6e5a39e842c7e9fdd9f50df9e86a9c52
Kiran-Karadia <68615802+Kiran-Karadia@users.noreply.github.com>
2021-10-06 17:37:38 +0100

    React routing and built some components

--- spotify_rooms_app/frontend/urls.py
+++ spotify_rooms_app/frontend/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import index
 
+
+# All of these urls link to index.html
+# This is because Django will to HTML which React then takes over and renders the correct page
 urlpatterns = [
-    path("", index)
+    path("", index),
+    path("join", index),
+    path("create", index)
 ]
