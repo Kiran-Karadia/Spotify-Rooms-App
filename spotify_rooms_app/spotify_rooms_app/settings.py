4358906d85eb1b17d0876e253a7e94f434740559
Kiran-Karadia <68615802+Kiran-Karadia@users.noreply.github.com>
2021-10-09 17:47:39 +0100

    Changed allowed hosts to * (all)

--- spotify_rooms_app/spotify_rooms_app/settings.py
+++ spotify_rooms_app/spotify_rooms_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uyk529!dxg4s=r=jf*&v7@&*s9bezll4elnnl%r7b@(j$qfila
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.42"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
